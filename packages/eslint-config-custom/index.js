f2dd37a365821c4a106aab922827a1dfdb96cbe8
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-10-07 14:16:25 -0400

    fix: updating eslint. Ref sn

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,23 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ["tailwindcss", "import", "simple-import-sort"],
+  extends: [
+    "plugin:tailwindcss/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "next",
+    "turbo",
+    "prettier",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/display-name": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
   },
 };
