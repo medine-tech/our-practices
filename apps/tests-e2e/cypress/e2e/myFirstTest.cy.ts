ecd085557cbfa4d7a710eddc9b12e1c2ceed2946
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-10-14 15:20:01 -0400

    feat: adding crypress

--- apps/tests-e2e/cypress/e2e/myFirstTest.cy.ts
+++ apps/tests-e2e/cypress/e2e/myFirstTest.cy.ts
@@ -1,5 +1,14 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
-    })
-})
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(true);
+    expect(false).to.equal(false);
+    cy.visit("https://master.pensanomica.com/clientes/flexio/login");
+    cy.request('POST', 'https://master.pensanomica.com/clientes/flexio/API_Flexio/login_master', { username: 'aaa' }).then(
+      (response) => {
+        // response.body is automatically serialized into JSON
+        console.log(response.body)
+        expect(response.body).to.have.property('success') // true
+      }
+    )
+  });
+});
